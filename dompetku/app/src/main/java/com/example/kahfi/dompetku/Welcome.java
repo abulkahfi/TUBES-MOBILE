9d429f44829f0ef8b19cb1780645f8772bafaf79
abulkahfi <60900117030@uin-alauddin.ac.id>
2020-01-07 19:41:56 +0800

    Kahfi

---
+++ dompetku/app/src/main/java/com/example/kahfi/dompetku/Welcome.java
@@ -0,0 +1,37 @@
+package com.example.kahfi.dompetku;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class Welcome extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void save(View v) {
+        TextView namaText = (TextView) findViewById(R.id.personal_name);
+        String nama = namaText.getText().toString();
+        User dataUser = new User(this);
+        dataUser.name = nama;
+        dataUser.amount = 0;
+        if (nama.equals("")) {
+            Toast.makeText(this, "Nama Tidak Boleh Kosong", Toast.LENGTH_LONG).show();
+        }
+        else {
+            dataUser.insertUser(dataUser);
+            Intent intent = new Intent(this, MainActivity.class);
+            this.startActivity(intent);
+        }
+    }
+}
